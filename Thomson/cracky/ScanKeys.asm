08304d50cc363f225a873270b09d828bbedff8a7
OlivierP-To8 <OlivierP.To8@free.fr>
2023-05-22 21:25:36 +0200

    Custom code for joystick input

--- Thomson/cracky/ScanKeys.asm
+++ Thomson/cracky/ScanKeys.asm
@@ -1,9 +1,9 @@
 include '../ThomsonTO.inc'
 
-Keys_Left equ $01
-Keys_Right equ $02
-Keys_Up equ $04
-Keys_Down equ $08
+Keys_Left equ $04
+Keys_Right equ $08
+Keys_Up equ $01
+Keys_Down equ $02
 Keys_Dir equ $0f
 Keys_Button0 equ $10
 Keys_Button1 equ $20
@@ -18,36 +18,19 @@ table:
     defb $0d, Keys_Button1 ; enter
     defb 0
 
-joypos:
-    defb $07, Keys_Left    ; ←
-    defb $03, Keys_Right   ; →
-    defb $01, Keys_Up      ; ↑
-    defb $05, Keys_Down    ; ↓
-    defb 0
-
 ScanKeys_: public ScanKeys_
     pshs b,x
 
         ; joystick test
-        clra ; A=0 => joystick 0
-        jsr JOYS ; 191 cycles
-        if cs ; if trigger button is pressed (carry set)
+        lda PRB1      ; get trigger data (if bit is 0)
+        anda #$40     ; keep b6 for trigger of joystick 0
+        if eq         ; if trigger button is pressed
             lda #Keys_Button0
         else
-            tstb ; B=0 => center of joystick, no direction
-            if ne
-                ldx #joypos
-                do
-                    lda ,x
-                while ne
-                    cmpb ,x+
-                    if eq
-                        lda ,x
-                        bra FIN
-                    endif
-                    leax 1,x
-                wend
-            else
+            lda PRA1  ; get direction data (if bit is 0)
+            coma      ; value of direction
+            anda #$0f ; only for joystick 0
+            if eq     ; if no direction
                 ldb $FFF0
                 cmpb #$01
                 if le ; if TO7 or TO7-70
