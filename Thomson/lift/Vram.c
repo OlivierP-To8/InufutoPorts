8297245ac0ec26b2cdbb383504ce45c969ef6e3f
OlivierP-To8 <OlivierP.To8@free.fr>
2023-09-26 20:26:38 +0200

    saved 318 bytes of cseg RAM on monochrome graphics

--- Thomson/lift/Vram.c
+++ Thomson/lift/Vram.c
@@ -12,7 +12,15 @@ void InitVram()
     MakeMono(Char_LiftBottom - Char_Logo, ColorRam + Char_Logo * CharHeight, 0x0e);
     MakeMono(Char_Roof - Char_LiftBottom, ColorRam + Char_LiftBottom * CharHeight, 0x05);
     MakeMono(Char_Man - Char_Roof,        ColorRam + Char_Roof * CharHeight, 0x01);
-    MakeColor(Char_End - Char_Man,        ColorRam + Char_Man * CharHeight);
+    MakeColor(Char_Monster - Char_Man,    ColorRam + Char_Man * CharHeight);
+    MakeMono(Char_Lift - Char_Monster,    ColorRam + Char_Monster * CharHeight, 0x06);
+    MakeMono(Char_Point - Char_Lift,      ColorRam + Char_Lift * CharHeight, 0x02);
+    MakeMono(Char_End - Char_Point,       ColorRam + Char_Point * CharHeight, 0x0f);
+    // Item flag color
+    MakeMono(1, ColorRam + Char_Item_A_Flag * CharHeight, 0x04);
+    MakeMono(1, ColorRam + Char_Item_B_Flag * CharHeight, 0x0a);
+    MakeMono(1, ColorRam + Char_Item_C_Flag * CharHeight, 0x0c);
+    MakeMono(1, ColorRam + Char_Item_D_Flag * CharHeight, 0x0d);
     ClearScreen();
 }
 
